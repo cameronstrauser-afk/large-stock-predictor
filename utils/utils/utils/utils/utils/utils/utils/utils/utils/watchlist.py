167aea9b64d01210cb9f5a359e2b35e1f974febf
cameronstrauser-afk <cameron.strauser@gmail.com>
2026-02-25 15:30:20 -0700

    Create watchlist.py

---
+++ utils/utils/utils/utils/utils/utils/utils/utils/utils/watchlist.py
@@ -0,0 +1,13 @@
+import json
+FILE="watchlist.json"
+
+def load_watchlist():
+    try:
+        with open(FILE,"r") as f:
+            return json.load(f)
+    except:
+        return []
+
+def save_watchlist(w):
+    with open(FILE,"w") as f:
+        json.dump(w,f)
